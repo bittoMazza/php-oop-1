ab3e7b25a819d70aad13b65e69651789fba38111
MyUsername <robertomazza99@gmail.com>
2022-09-02 13:25:56 +0200

    creation repo

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+class Movie{
+
+    private $title;
+    private $director;
+    private $catergory;
+    private $duration;
+
+    /**
+     *
+     * Get title from movie 
+     *
+     * @return $title   
+     *
+    */
+    public function GetTitle(){
+        return $this->title;
+    }
+
+    /**
+     *
+     * Get title from movie 
+     *
+     * @return $title   
+     *
+    */
+    public function GetDirector(){
+        return $this->director;
+    }
+
+    /**
+     *
+     * Get title from movie 
+     *
+     * @return $title   
+     *
+    */
+    public function GetCategory(){
+        return $this->catergory;
+    }
+    
+     /**
+     *
+     * Get title from movie 
+     *
+     * @return $title   
+     *
+    */
+    public function GetDuration(){
+        return $this->duration;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Movie</title>
+</head>
+<body>
+    
+</body>
+</html>
